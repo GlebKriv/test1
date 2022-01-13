8c4f3a03f7e39f0d237975e53ebd64b9bfb89664
GlebKriv <97518848+GlebKriv@users.noreply.github.com>
2022-01-13 13:38:19 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const fileinclude = require('gulp-file-include');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
-const webserver = require('webserver');
+const webserver = require('webserver').create();
 const rimraf = require('rimraf');
 const comments = require('gulp-header-comment');
 
